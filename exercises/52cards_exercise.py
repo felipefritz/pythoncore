6ab54e7348b77ad3a8cb4c65e82121dc075425dc
Felipe Del Fierro <felipe.delfierro@globant.com>
2022-08-10 13:02:03 -0400

    52cards: changed deck to slack

--- exercises/52cards_exercise.py
+++ exercises/52cards_exercise.py
@@ -4,38 +4,38 @@ import random
 class Cards:
 
     def __init__(self):
-        self.types = list('HSCD')
+        self.types = list('HCDS')
         self.cards = [i for i in range(2, 11)] + list('JQKA')
-        self.deck = [(type, card) for type in self.types for card in self.cards]
+        self.stack = [(type, card) for type in self.types for card in self.cards]
 
-    def shuffle_deck(self):
-        random.shuffle(self.deck)
+    def shuffle_stack(self):
+        random.shuffle(self.stack)
 
     def deal_cards(self, q: int):
-        self.sample = random.sample(self.deck, q)
-        [self.deck.remove(a) for a in self.sample]
+        self.sample = random.sample(self.stack, q)
+        [self.stack.remove(a) for a in self.sample]
 
     def draw_card(self):
-        self.card = self.deck.pop()
+        self.card = self.stack.pop()
 
-    def restore_suffle_deck(self):
-        self.deck = Cards().deck
-        random.shuffle(self.deck)
+    def restore_suffle_stack(self):
+        self.stack = Cards().stack
+        random.shuffle(self.stack)
 
 
 
 c = Cards()
-print(c.deck)
+print(c.stack)
 
-c.shuffle_deck()
-print(c.deck)
+c.shuffle_stack()
+print(c.stack)
 
 c.deal_cards(10)
 print(c.sample)
 
 c.draw_card()
 print(c.card)
-print(c.deck)
+print(c.stack)
 
-c.restore_suffle_deck()
-print(c.deck)
+c.restore_suffle_stack()
+print(c.stack)
